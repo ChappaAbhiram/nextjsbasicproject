fa394c3a022bfde352a565ebb88af34212d2e9b7
ChappaAbhiram <abhiramchappa@gmail.com>
2024-02-04 19:22:58 +0530

    Navigation done with Link element

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,4 +1,23 @@
+import Link from "next/link";
 function Aboutpage(){
-    return <h1>About us</h1>
+    const developers = [
+        { id: 1, name: 'Yash', role: 'Senior Developer' },
+        { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+        { id: 3, name: 'Suresh', role: 'Frontend Developer' },
+      ];
+      return (
+        <div>
+          <h1>Developer List</h1>
+          <ul>
+            {developers.map(dev => (
+              <li key={dev.id}>
+                <Link href={`/aboutus/${dev.id}`}>
+                {dev.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
 }
 export default Aboutpage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,13 @@
 //our-domain.com/news
+import { Fragment } from "react";
+import Link from "next/link";
 function Newspage(){
-    return <h1>The News page</h1>
+    return <Fragment>
+        <h1>The News page</h1>
+        <ul>
+            <li><Link href="/news/nextjs-is-good">NextJs is good</Link></li>
+            <li><Link href="/news/somethingelse">Something else</Link></li>
+        </ul>
+        </Fragment>
 }
 export default Newspage;
